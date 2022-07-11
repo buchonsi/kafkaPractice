d2f0164ab40f605537989fac9716ca69f59f494f
buchonsi <buchonsi@naver.com>
2022-07-12 00:51:02 +0900

    카프카 프로듀서 컨슈머 테스트

---
+++ src/main/java/com/example/consumer/RebalanceConsumer.java
@@ -0,0 +1,54 @@
+package com.example.consumer;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.*;
+
+public class RebalanceConsumer {
+    private final static Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+    private final static String GROUP_ID = "test-group";
+    private static KafkaConsumer<String, String> consumer;
+    private static Map<TopicPartition, OffsetAndMetadata> currentOffset = new HashMap<>();
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        consumer = new KafkaConsumer<String, String>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("{}", record);
+                currentOffset.put(new TopicPartition(record.topic(), record.partition()), new OffsetAndMetadata(record.offset() + 1, null));
+                consumer.commitSync();
+            }
+        }
+    }
+
+    private static class RebalanceListener implements ConsumerRebalanceListener {
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            logger.warn("Partitions are assgined");
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            logger.warn("Partitions are revoked");
+            consumer.commitSync(currentOffset);
+        }
+    }
+}
+
+
+
